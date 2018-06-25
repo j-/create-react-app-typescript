3b0014fb926361a151a6dafb99d7643f77ca7ff3
j- <j@skeoh.com>
2018-06-25 23:01:16 +1000

    Update package template scripts

--- scripts/init.js
+++ scripts/init.js
@@ -40,10 +40,10 @@ module.exports = function(
 
   // Setup the script rules
   appPackage.scripts = {
-    start: 'react-scripts-ts start',
-    build: 'react-scripts-ts build',
-    test: 'react-scripts-ts test --env=jsdom',
-    eject: 'react-scripts-ts eject',
+    start: 'react-scripts-j start',
+    build: 'react-scripts-j build',
+    test: 'react-scripts-j test --env=jsdom',
+    eject: 'react-scripts-j eject',
   };
 
   fs.writeFileSync(
